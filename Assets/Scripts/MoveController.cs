a4935df98542554c82a1412996aa9deb8db4d09c
GeekmasterK <ktheiss1990@gmail.com>
2021-09-26 12:15:32 -0700

    Added functions to print the instructions to the console and move the player.

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -9,11 +9,23 @@ public class MoveController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with the WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
     {
         float xMove = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zMove = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
